a3ad807e8392dcbac5ecbff1686591ae82a5c706
RodrigoPiresgames <Rodrigo.Pires.gamesdesign@gmail.com>
2023-03-31 13:44:27 +0100

    Add stopwatch

---
+++ Tempo/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Diagnostics;
+using System.Threading;
+
+namespace Tempo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Stopwatch crono1 = new Stopwatch();
+            Stopwatch crono2 = new Stopwatch();
+
+            crono1.Start();
+
+            Thread.Sleep(500);
+
+            crono2.Start();
+
+            Thread.Sleep(300);
+
+            crono1.Stop();
+            crono2.Stop();
+        }
+    }
+}
