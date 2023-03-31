ae1f5a5316adb42cc14a7f7c8dced6f3fac4cecd
RodrigoPiresgames <Rodrigo.Pires.gamesdesign@gmail.com>
2023-03-31 14:59:05 +0100

    Add console write with total

--- RandomDice/Program.cs
+++ RandomDice/Program.cs
@@ -12,8 +12,16 @@ namespace RandomDice
             string str = Console.ReadLine();
             int numOfDice = Convert.ToInt32(str);
 
+            int result = 0;
+
             for (int x = 0; x <= numOfDice - 1; x++)
-                Console.WriteLine("{0}", rand.Next(7));
+            {
+                int diceValue = rand.Next(1, 7);
+
+                result += diceValue;
+            }
+
+            Console.WriteLine(result);
         }
     }
 }
