2f8a45e28bf9a6f2a9cc3403b1bb01403982b2f3
RodrigoPiresgames <Rodrigo.Pires.gamesdesign@gmail.com>
2023-03-31 14:52:06 +0100

    Add random values

--- RandomDice/Program.cs
+++ RandomDice/Program.cs
@@ -6,7 +6,14 @@ namespace RandomDice
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            var rand = new Random();
+
+            Console.WriteLine("Gimme numb");
+            string str = Console.ReadLine();
+            int numOfDice = Convert.ToInt32(str);
+
+            for (int x = 0; x <= numOfDice - 1; x++)
+                Console.WriteLine("{0}", rand.Next(7));
         }
     }
 }
